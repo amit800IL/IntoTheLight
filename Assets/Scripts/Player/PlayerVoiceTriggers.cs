181851c5f7bcf84d02ccc864b8c8431717122d30
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-06-24 17:06:52 +0300

    Updates to Hllucinations System

--- Assets/Scripts/Player/PlayerVoiceTriggers.cs
+++ Assets/Scripts/Player/PlayerVoiceTriggers.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerVoiceTriggers : MonoBehaviour
@@ -27,30 +28,52 @@ public class PlayerVoiceTriggers : MonoBehaviour
 
     private IEnumerator GuardOn(Collider other)
     {
-        List<VoiceTriggers> activeTriggers = new List<VoiceTriggers>();
+        int triggersToActivate = 3;
+        float triggerRadius = 5f;
+        float deactivationDelay = 3f; 
 
+
+        Dictionary<VoiceTriggers, float> triggerDistances = new Dictionary<VoiceTriggers, float>();
         foreach (VoiceTriggers voiceTrigger in voiceTriggers)
         {
-            if (other.gameObject.CompareTag("SoundTrigger"))
+            float distance = Vector3.Distance(transform.position, voiceTrigger.transform.position);
+            triggerDistances.Add(voiceTrigger, distance);
+        }
+
+        List<VoiceTriggers> sortedTriggers = triggerDistances.Keys.OrderBy(t => triggerDistances[t]).ToList();
+
+        int activatedTriggers = 0;
+        List<VoiceTriggers> activeTriggers = new List<VoiceTriggers>();
+        foreach (VoiceTriggers voiceTrigger in sortedTriggers)
+        {
+            float distanceToTrigger = Vector3.Distance(transform.position, voiceTrigger.transform.position);
+            if (distanceToTrigger <= triggerRadius && activatedTriggers < triggersToActivate)
             {
-                if (Random.value < 0.5f)
-                {
-                    activeTriggers.Add(voiceTrigger);
-                    voiceTrigger.audioSource.Play();
-                    voiceTrigger.meshRenderer.forceRenderingOff = false;
-                    voiceTrigger.meshRenderer2.forceRenderingOff = false;
-                }
+                voiceTrigger.audioSource.Play();
+                voiceTrigger.meshRenderer.forceRenderingOff = false;
+                voiceTrigger.meshRenderer2.forceRenderingOff = false;
+                activatedTriggers++;
+                activeTriggers.Add(voiceTrigger);
             }
         }
 
-        yield return new WaitForSeconds(3);
-
-        foreach (VoiceTriggers voiceTrigger in activeTriggers)
+        while (activeTriggers.Count > 0)
         {
-            voiceTrigger.audioSource.Stop();
-            voiceTrigger.meshRenderer.forceRenderingOff = true;
-            voiceTrigger.meshRenderer2.forceRenderingOff = true;
-        }
+            for (int i = activeTriggers.Count - 1; i >= 0; i--)
+            {
+                VoiceTriggers voiceTrigger = activeTriggers[i];
+                float distanceToTrigger = Vector3.Distance(transform.position, voiceTrigger.transform.position);
+                if (distanceToTrigger > triggerRadius)
+                {
+                    yield return new WaitForSeconds(deactivationDelay);
+                    voiceTrigger.audioSource.Stop();
+                    voiceTrigger.meshRenderer.forceRenderingOff = true;
+                    voiceTrigger.meshRenderer2.forceRenderingOff = true;
+                    activeTriggers.RemoveAt(i);
+                }
+            }
+            yield return null;
+        } 
     }
 
     private IEnumerator GuardOff(Collider other)
