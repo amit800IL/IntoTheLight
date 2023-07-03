fb7a499f70a338905b3b4300dfe63da4ecf17bb4
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-07-03 11:11:10 +0300

    Pasue menu fix

--- Assets/Scripts/Ui/PauseMenu.cs
+++ Assets/Scripts/Ui/PauseMenu.cs
@@ -1,19 +1,33 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool gameIsPaused;
-
+    private bool keyPress;
+    public GameObject OnPause;
+    private void Update()
+    {
+        keyPress = Keyboard.current.escapeKey.isPressed;
+        PauseGame();
+    }
     public void PauseGame()
     {
-        Time.timeScale = 0f;
-        gameIsPaused = true;
+        if (keyPress)
+        {
+            Time.timeScale = 0f;
+            gameIsPaused = true;
+            OnPause.SetActive(true);
+        }
 
     }
     public void Resume()
     {
         Time.timeScale = 1;
-        gameIsPaused = true;
+        gameIsPaused = false;
+        OnPause.SetActive(false);
     }
     public void Close()
     {
